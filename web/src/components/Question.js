05e87c8fbd79e6048d3a28e0410bc17665ae04eb
Leonardo <leocv0205@gmail.com>
2021-10-22 11:44:13 -0500

    Se crea el campo para filtrar

--- web/src/components/Question.js
+++ web/src/components/Question.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Question = ({ question, excerpt, onDelete }) => (
-  <article className={excerpt ? 'question-excerpt' : 'question'}>
-    <div  dangerouslySetInnerHTML={{__html:question.question}} />
-    {/* <h2>{question.question}</h2> */}
-    <p>{question.category}  - <small>{question.type}</small></p>
-   
-    {onDelete && (
-      <button className="button right" onClick={() => onDelete(question.id)}>DELETE</button>
-    )}
-    {excerpt && (
-      <Link to={`/question/${question.id}`} className="button">
-        View Question
-      </Link>
-    )}
-  </article>
-)
+export const Question = ({ question, excerpt, onDelete, setCategorySearch }) => {
+
+
+  const handleCategorySearch = (e)=>{
+    setCategorySearch(question.category)
+  }
+
+  return <article className={excerpt ? 'question-excerpt' : 'question'}>
+  <h2><div dangerouslySetInnerHTML={{__html:question.question}} /></h2>
+  {setCategorySearch?
+  <p className="categorySearch" onClick={handleCategorySearch}>{question.category}  - <small>{question.type}</small></p>:
+  <p>{question.category}  - <small>{question.type}</small></p>}
+
+  {onDelete && (
+    <button className="button right" onClick={() => onDelete(question.id)}>DELETE</button>
+  )}
+  {excerpt && (
+    <Link to={`/question/${question.id}`} className="button ">
+      View Question
+    </Link>
+  )}
+</article>
+}
--- web/src/pages/QuestionsPage.js
+++ web/src/pages/QuestionsPage.js
@@ -1,24 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchQuestions } from '../actions/questionActions'
 import { Question } from '../components/Question'
+import { Link } from 'react-router-dom'
 
 const QuestionsPage = ({ dispatch, loading, questions, hasErrors }) => {
     useEffect(() => {
         dispatch(fetchQuestions())
     }, [dispatch])
+
+    const [search, setSearch] = useState('');
+    const [categorySearch, setCategorySearch] = useState('');
+
+    const questionFilteredCategory = questions.filter(question => question.category.toUpperCase().includes(categorySearch.toUpperCase()))
+
+    const questionsFilteredSearch = questionFilteredCategory.filter(question => question.question.toUpperCase().includes(search.toUpperCase()))
+
+    const goTOVariable = questionsFilteredSearch[0]?.id
+    //const questionDirected = questionsFilteredSearch[0]?.question
     
+
+
+    const handleSearch =(e)=>{
+        setSearch(e.target.value);
+    }
+     
     const renderQuestions = () => {
         if (loading) return <p>Loading questions...</p>
         if (hasErrors) return <p>Unable to display questions.</p>
 
-        return questions.map(question => <Question key={question.id} question={question} excerpt />)
+        return questionsFilteredSearch.map(question => <Question setCategorySearch={setCategorySearch}
+             key={question.id} 
+             question={question} excerpt />)
     }
 
     return (
-        <section>
+        <section className="page-section">
             <h1>Questions</h1>
+            <form>
+                <input type="text" onChange={handleSearch}/>
+                <Link to={`/question/${goTOVariable}`} ><input style={{display: 'none'}} type="submit" value="search" /></Link>
+            </form>
             {renderQuestions()}
         </section>
     )
